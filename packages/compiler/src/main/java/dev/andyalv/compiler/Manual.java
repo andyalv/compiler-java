27a74bfb7ecce3712d569740cd7c3f8474c09607
andyalv <andy.red02@gmail.com>
2026-04-17 09:49:11 -0600

    feat: refactor main method to use CompilerFacade and add example SQL inputs

--- packages/compiler/src/main/java/dev/andyalv/compiler/Manual.java
+++ packages/compiler/src/main/java/dev/andyalv/compiler/Manual.java
@@ -4,11 +4,46 @@ import org.antlr.runtime.*;
 
 public class Manual {
     public static void main(String[] args) throws Exception {
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-        rootLexer lexer = new rootLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        rootParser parser = new rootParser(tokens);
-        parser.start();
-        System.out.println(parser.getSQL());
+        CompilerFacade facade = new CompilerFacade();
+        String correct_input = """
+                create database enterprise
+                with enterprise
+                table depto
+                    start
+                       name string 40
+                       department_code string 10
+                       number   integer
+                       responsabilities string
+                    end\s
+                table empleado
+                    start
+                       name string 40
+                       age integer
+                       birth_date date
+                       salary integer
+                       relationship depto
+                    end
+                closecon
+                """;
+        String relationship_error = """
+                create database enterprise
+                with enterprise
+                table empleado
+                    start
+                       relationship depto
+                    end
+                closecon
+                """;
+        String sintax_error = """
+                create database enterprise
+                with enterprise
+                table empleado
+                    start
+                       salary numeric 6,
+                    end
+                closecon
+                """;
+        String sql = facade.compile(sintax_error);
+        System.out.println(sql);
     }
 }
